19ab3d7e5fa424d52be8dd75714955bbf8f8d0f8
TYGER <boglbbogl@gmail.com>
2025-07-15 11:10:46 +0900

    commit

--- map_playground/lib/home_page.dart
+++ map_playground/lib/home_page.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String _mapStyle = '''
+  [
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        { "color": "#cccccc" }
+      ]
+    }
+  ]
+  ''';
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
-          zoom: 6,
+          zoom: 20,
           target: LatLng(37.5642135, 127.0016985),
         ),
+        style: '''
+  [
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    }
+  ]
+  ''',
       ),
     );
   }
