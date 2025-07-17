51cc95c23d02c5c059e498486c2be33435e922bb
TYGER <boglbbogl@gmail.com>
2025-07-17 15:23:09 +0900

    commit

--- map_playground/lib/google/google_map_page.dart
+++ map_playground/lib/google/google_map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapPage extends StatefulWidget {
@@ -9,31 +10,55 @@ class GoogleMapPage extends StatefulWidget {
 }
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
+  GoogleMapController? _controller;
+
+  Future<void> _moveCurrentLocation() async {
+    final Position position = await Geolocator.getCurrentPosition();
+
+    _controller?.animateCamera(
+      CameraUpdate.newLatLng(LatLng(position.latitude, position.longitude)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GoogleMap(
-          myLocationEnabled: true,
-
-          initialCameraPosition: CameraPosition(
-            zoom: 20,
-            target: LatLng(37.5642135, 127.0016985),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: (GoogleMapController controller) =>
+                _controller = controller,
+            initialCameraPosition: const CameraPosition(
+              zoom: 20,
+              target: LatLng(37.5642135, 127.0016985),
+            ),
+            myLocationButtonEnabled: false,
+            myLocationEnabled: false,
+            zoomControlsEnabled: false,
           ),
-          // markers: {
-          //   Marker(
-          //     markerId: MarkerId("test"),
-          //     position: LatLng(37.5665, 126.9780),
-          //   )
-          // },
-          // minMaxZoomPreference: MinMaxZoomPreference(1, 4),
-          // cameraTargetBounds: CameraTargetBounds(
-          //   LatLngBounds(
-          //     southwest: LatLng(33.0, 124.0),
-          //     northeast: LatLng(39.5, 131.0),
-          //   ),
-          // ),
-        ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: SafeArea(
+              child: GestureDetector(
+                onTap: () => _moveCurrentLocation(),
+                child: Container(
+                  width: 72,
+                  height: 72,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(72),
+                    color: Colors.black.withOpacity(0.7),
+                  ),
+                  child: Icon(
+                    Icons.my_location_rounded,
+                    size: 36,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
